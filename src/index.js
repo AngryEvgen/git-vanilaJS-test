9b786c653a90f8d039f4e9143d1dd963081f1baf
AngryEvgen <jekyll7391@gmail.com>
2021-11-10 23:56:33 +0400

    added some features

--- src/index.js
+++ src/index.js
@@ -1,48 +1,55 @@
 'use strict';
+// Получаем данные
+async function getData(path) {
+  try {
+    const res = await fetch(path);
+    return res.json();
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+// Рапсределяем персонажей по ячейкам
+function setCharacters(arr, tds) {
+  for (let i = 0; i < arr.length; i++) {
+    let td = tds[i];
+    let item = arr[i];
+
+    td.innerHTML = item.name;
+  }
+}
+
+// Добавляем обработчик событий на каждую ячейку
+function handleClick(arr, cells, element) {
+  for (let i = 0; i < cells.length; i++) {
+    let td = cells[i];
+
+    td.addEventListener('click', function () {
+      let result = arr[i];
+
+      element.innerHTML = `<li>Name: ${result.name}</li>
+      <li>Height: ${result.height}</li>
+      <li>Mass: ${result.mass}</li>
+      <li>Hair color: ${result.hair_color}</li>
+      <li>Skin color: ${result.skin_color}</li>
+      <li>Eye color: ${result.eye_color}</li>
+      <li>Birth year: ${result.birth_year}</li>
+      <li>Gender: ${result.gender}</li>`;
+    });
+  }
+}
+
+// Инициализация приложения
+async function initApp(path, cells, element) {
+  const { results } = await getData(path);
+  setCharacters(results, cells);
+  handleClick(results, cells, element);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  let url = 'https://swapi.dev/api/people/';
+  const URL = 'https://swapi.dev/api/people/';
   let tds = document.querySelectorAll('td');
   let info = document.querySelector('.info');
-  // Получаем данные
-  async function getData(path) {
-    try {
-      const res = await fetch(path);
-      return res.json();
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-  // Рапсределяем персонажей по ячейкам
-  function setCharacters(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      let td = tds[i];
-      let item = arr[i];
 
-      td.innerHTML = item.name;
-    }
-  }
-  // Инициализация приложения
-  async function initApp(url) {
-    const { results } = await getData(url);
-
-    setCharacters(results);
-
-    for (let i = 0; i < tds.length; i++) {
-      let td = tds[i];
-
-      td.addEventListener('click', function () {
-        let result = results[i];
-
-        info.innerHTML = `<li>Name: ${result.name}</li>
-          <li>Height: ${result.height}</li>
-          <li>Mass: ${result.mass}</li>
-          <li>Hair color: ${result.hair_color}</li>
-          <li>Skin color: ${result.skin_color}</li>
-          <li>Eye color: ${result.eye_color}</li>
-          <li>Birth year: ${result.birth_year}</li>
-          <li>Gender: ${result.gender}</li>`;
-      });
-    }
-  }
-  initApp(url);
+  initApp(URL, tds, info);
 });
